b1a0fd162a6c882bbac966f516dadbb4e32b3520
Sahan Dhanujaya <sahandhanujaya18@gmail.com>
2026-07-22 22:36:32 +0530

    update: server.js

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.get("/", (req, res) => {
 
 app.get("/api/fake", async (req, res) => {
   await fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then(response => response.json())
-  .then(data => res.send(data));
+  .then((response) => {
+    const data = response.json()
+    res.send(data)
+  })
 });
 
 mongoose
